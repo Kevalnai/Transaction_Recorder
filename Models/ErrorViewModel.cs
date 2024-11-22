7716316ad177eb73dbb7a7eaf45ac24349730412
Keval <kevalnai1504@gmail.com>
2024-11-21 20:26:08 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Transaction_Recorder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
